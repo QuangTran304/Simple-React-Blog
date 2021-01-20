d05adda4fc6c502c7d79751458c660413d0d9bbc
Quang <nhatquang304@gmail.com>
2021-01-20 21:39:27 +0000

    Create a fake REST API server with JSON server

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,10 @@ const Home = () => {
     const tempBlogs = blogs.filter( (blog) => blog.id !== id );
     setBlogs(tempBlogs);
   }
+
+  useEffect( () => {
+    console.log('Use effect ran');
+  }, []);
 
   return (  
     <div className="home">
