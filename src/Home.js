ba1a6119531a123f73903d154dc40c4743661dba
Quang <nhatquang304@gmail.com>
2021-01-20 21:58:29 +0000

    Add method to fetch data from our local JSON server whenever the component is rendered

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,21 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My First Blog', body: 'This is my very very first blog made with React...', author: 'Quang', id: 1 },
-    { title: 'Getting Started With React', body: 'This is my very very first blog made with React...', author: 'Josh', id: 2 },
-    { title: 'How to Web Dev', body: 'This is my very very first blog made with React...', author: 'Maria', id: 3 }
-  ]);
-  
-  const handleDelete = (id) => {
-    const tempBlogs = blogs.filter( (blog) => blog.id !== id );
-    setBlogs(tempBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
 
   useEffect( () => {
-    console.log('Use effect ran');
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then( (data) => {
+        setBlogs(data);
+      })
   }, []);
 
   return (  
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
-      <br /> <br />
-      <BlogList blogs={blogs.filter( (blog) => blog.author === 'Quang' )} title='Quang blogs'/>
+      {blogs && <BlogList blogs={blogs} title='All Blogs' />}
     </div>
   );
 }
