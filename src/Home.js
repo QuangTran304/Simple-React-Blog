d1a1970c55956f6ce7949edda5a4390f5d1e27a6
Quang <nhatquang304@gmail.com>
2021-01-24 07:02:43 +0000

    Create server error handlings while fetching data

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,36 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
 
   useEffect( () => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout( () => {   // Used only for this project to mimic the time load in the real world
+      fetch('http://localhost:8000/blogs/')
       .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data from server');
+        }
         return res.json();
       })
-      .then( (data) => {
+      .then( (data) => {  // If error happened but later we could fetch data again, set Error back to 'null'
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
       })
+      .catch(err => {     // If the error happened, we don't want to show the word "Loading..."
+        setIsLoading(false);
+        setError(err.message);
+      });
+    }, 1000);   // Wait for an additional of 1 second
   }, []);
 
   return (  
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title='All Blogs' />}
+      { error  &&  <div>{ error }</div> }
+      { isLoading  &&  <div>Loading...</div> }
+      { blogs  &&  <BlogList blogs={ blogs } title='All Blogs' /> }
     </div>
   );
 }
