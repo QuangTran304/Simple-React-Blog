058d2c15068f735117d21e871432d51c59575f82
Quang <nhatquang304@gmail.com>
2021-01-28 06:32:30 +0000

    Add 'useHistory' hook to redirect user to Home page after done writing blog

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('quang');
   const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,8 @@ const Create = () => {
       console.log('NEW BLOG ADDED');
       setIsLoading(false);
     })
+
+    history.push('/');    // Redirect user back to home page
   }
 
   return (  
