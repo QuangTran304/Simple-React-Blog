ce801d835e70e1473c546b4062eddc0cab010b94
Quang <nhatquang304@gmail.com>
2021-01-28 06:09:45 +0000

    Add form to create new blogs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('quang');
+
   return (  
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title: </label>
+        <input 
+          type="text"
+          required
+          value={ title }
+          onChange={ (e) => setTitle(e.target.value) }
+        />
+
+        <label>Blog body: </label>
+        <textarea 
+          required
+          value={ body }
+          onChange={ (e) => setBody(e.target.value) }
+        ></textarea>
+
+        <label>Blog author:</label>
+        <select value={ author } onChange={ (e) => setAuthor(e.target.value) }>
+          <option value="quang">Quang</option>
+          <option value="maria">Maria</option>
+          <option value="josh">Josh</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
